7cda17498850b06064bd20b80a7ac31ea7271494
ibrahim-daoussi <ibrahim.daoussi@gmail.com>
2025-06-11 12:44:34 +0200

    update the html file

--- src/prompt.py
+++ src/prompt.py
@@ -1,18 +1,10 @@
 prompt_template="""
-You are a helpful and professional medical chatbot.
+Answer the question using only the relevant information from the provided context.
+If the context does not contain relevant information to answer the question, respond with "I don't know."
+Do not use any outside knowledge or make assumptions.
 
-Use only the information provided in the context below to answer the question. Do not use any external knowledge, make assumptions, or infer information not present in the context.
-
-If the context does not contain enough information to answer the question, reply with:
-"I’m sorry, I don’t have enough information to answer that based on the provided content."
-
-Maintain a concise, clear, and respectful tone in all responses.
-
-Context:
-{context}
-
-Question:
-{question}
+Context: {context}
+Question: {question}
 
 Answer:
 """
