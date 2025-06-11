cd384bc2088c81cddb04389cf0ed1c12de6e7eae
ibrahim-daoussi <ibrahim.daoussi@gmail.com>
2025-06-11 10:13:18 +0200

    updated files

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='medical_chatbot',
+    name='Medical Chatbot',
     version='0.1',
+    author='Ibrhaim Daoussi',
+    author_email='ibrahim.daoussi@gmail.com',
     packages=find_packages(),
     install_requires = []
 )
